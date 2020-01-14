41d589df43a2201e3d1d9cf173a4b60fd55024bb
hutuguaner <553761200@qq.com>
2020-01-14 17:04:21 +0800

    dev

---
+++ app/src/main/java/com/lzq/tuliaoand/activity/BroadCastDetailActivity.java
@@ -0,0 +1,16 @@
+package com.lzq.tuliaoand.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lzq.tuliaoand.R;
+
+public class BroadCastDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast_detail);
+    }
+}
